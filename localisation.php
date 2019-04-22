c8e1e84708ff675c6e98e4c59ec774a8077c4531
Hugo <hfief1806@gmail.com>
2019-04-22 19:23:17 +0200

    suppression commentaires inutiles

--- localisation.php
+++ localisation.php
@@ -105,40 +105,12 @@ $bdd = new PDO('mysql:host=localhost;dbname=tsti2d1;charset=utf8','root','');
 
     </script>  
   
-        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-	async defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer>
+    </script>
 
 	<?php include("footer.php"); ?>
 
 </body>
 
 </html>
-
-<!--
-        var infoWindow = new google.maps.InfoWindow({map: map});
-        AIzaSyAAFAkT5xTQMBWh8_Gf1Okn0liXsuAnl_g
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Localisation Trouvée');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-       
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
--->
